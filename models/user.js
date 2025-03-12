97e90a3de8b58e7e5ab22f1bab310e337dca7117
roydhirj19 <dhirajyadav1604@gmail.com>
2025-03-12 10:22:11 +0530

    made in user email is unquie

--- models/user.js
+++ models/user.js
@@ -5,6 +5,7 @@ const passportLocalMongoose = require('passport-local-mongoose');
 const userSchema = new Schema({
     email:{
         type: String,
+        unique:true,
         required: true,
     }
 });
